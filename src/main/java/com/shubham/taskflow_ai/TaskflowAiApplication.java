60ef3474a3f186657be6604d333f5875988ab3fb
Shubham Shelar <85821105+shubhamshelar@users.noreply.github.com>
2025-08-20 02:01:04 +0530

    SETUP1: Gradle Project Setup

---
+++ src/main/java/com/shubham/taskflow_ai/TaskflowAiApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.taskflow_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskflowAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubham/taskflow_ai/TaskflowAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.taskflow_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
